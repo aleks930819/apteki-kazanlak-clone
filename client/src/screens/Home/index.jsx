68616bc27b2f3de4ee61d98dffbcb797eec4ad53
Aleksandar Atanasov <aleks930819@gmail.com>
2023-06-23 11:21:50 +0300

    fix

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -1,11 +1,13 @@
 import Hero from '../../components/Hero';
 import ServiceHighlights from '../../components/ServiceHighlights';
+import Maps from '../../components/Maps/Maps';
 
 const HomeScreen = () => {
   return (
     <main>
       <Hero />
       <ServiceHighlights />
+      <Maps />
     </main>
   );
 };
--- client/src/screens/Promo/index.jsx
+++ client/src/screens/Promo/index.jsx
@@ -1,62 +1,34 @@
-import Ferveks from '../../assets/ferveks.jpeg';
-import Magne from '../../assets/magne.png';
+import { useLoaderData } from 'react-router-dom';
 import PromoCard from '../../components/Promo/PromoCard';
-
-const promoData = [
-  {
-    id: 1,
-    title: 'Фервекс за възрастни прахчета х 8',
-    imgURL: Ferveks,
-    newPrice: '5.99',
-    oldPrice: '7.99',
-    summary: 'Прахчета за възрастни при простуда и грип',
-  },
-  {
-    id: 2,
-    title: 'Магне Б6 Форте табл.',
-    imgURL: Magne,
-    newPrice: '5.99',
-    oldPrice: '7.99',
-    summary: 'Подпомага намаляването на умората и изтощението',
-  },
-  {
-    id: 4,
-    title: 'Магне Б6 Форте табл.',
-    imgURL: Magne,
-    newPrice: '5.99',
-    oldPrice: '7.99',
-    summary: 'Подпомага намаляването на умората и изтощението',
-  },
-  {
-    id: 6,
-    title: 'Магне Б6 Форте табл.',
-    imgURL: Magne,
-    newPrice: '5.19',
-    oldPrice: '5.92',
-    summary: 'Подпомага намаляването на умората и изтощението',
-  },
-];
+import { getPromoProducts } from '../../services/apiPromoProducts';
 
 const PromoScreen = () => {
+  const promoData = useLoaderData();
+
   return (
-    <div className="mx-auto  w-[75vw] mt-5 ">
+    <div className="mx-auto  mt-[100px] w-[75vw] ">
       <ul
-        className="grid grid-cols-1  items-center  sm:grid-cols-4 gap-2
+        className="grid grid-cols-1  items-center  gap-2 sm:grid-cols-4
       "
       >
         {promoData.map((item) => (
           <PromoCard
             key={item.id}
             title={item.title}
-            imgURL={item.imgURL}
+            image={item.image}
             newPrice={item.newPrice}
             oldPrice={item.oldPrice}
-            summary={item.summary}
+            description={item.description}
           />
         ))}
       </ul>
     </div>
   );
+};
+
+export const loader = async () => {
+  const promoProducts = await getPromoProducts();
+  return promoProducts;
 };
 
 export default PromoScreen;
