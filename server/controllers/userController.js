c3a6e18ffccf972ce31d62c3471445dbb097fb31
Aleksandar Atanasov <aleks930819@gmail.com>
2023-06-24 13:57:38 +0300

    added auth login on the server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,20 @@ import User from '../models/userModel.js';
 // @route POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send('Login Route');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.comparePassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
 });
 
 // @desc Logout user / clear cookie
