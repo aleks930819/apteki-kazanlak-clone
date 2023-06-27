3cd90b903b7412ffecd3d21e6f8c07e056b19534
Aleksandar Atanasov <aleks930819@gmail.com>
2023-06-27 21:27:37 +0300

    changed logic

--- client/src/components/Maps/LinkElement.jsx
+++ client/src/components/Maps/LinkElement.jsx
@@ -2,17 +2,12 @@ import logo from '../../assets/logo.png';
 
 const LinkElement = ({ name, isActive, onClick, activatedItemId }) => {
   const linkClasses = `pl-[15px] bg-no-repeat bg-contain flex cursor-pointer items-center gap-2 text-base font-semibold text-primary ${
-    isActive ? '' : 'opacity-50'
+    isActive || activatedItemId === null ? 'opacity-1' : 'opacity-50'
   }`;
-
-  const allActiveClass =
-    'pl-[15px] bg-no-repeat bg-contain flex cursor-pointer items-center gap-2 text-base font-semibold text-primary opacity-1';
-
-  const isActiveItem = activatedItemId === null ? allActiveClass : linkClasses;
 
   return (
     <li
-      className={isActiveItem}
+      className={linkClasses}
       style={{ backgroundImage: `url(${logo})` }}
       onClick={onClick}
     >
