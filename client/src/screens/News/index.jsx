26f8311761925629c79b93574ecad9707767f5a4
Aleksandar Atanasov <aleks930819@gmail.com>
2023-06-28 18:48:24 +0300

    fixed style

--- client/src/screens/News/index.jsx
+++ client/src/screens/News/index.jsx
@@ -31,9 +31,9 @@ const NewsScreen = () => {
   return (
     <>
       <NewsBanner image={image.url} />
-      <section className="mx-auto bg-white px-[70px] py-[30px] lg:w-[75vw]">
+      <section className="mx-auto bg-white px-[40px] py-[20px] sm:py-[30px] lg:w-[75vw]">
         <div className="flex flex-col items-start justify-center">
-          <h1 className="mb-4 text-4xl font-semibold text-gray-800">{title}</h1>
+          <h1 className="mb-4 text-2xl sm:text-4xl font-semibold text-gray-800">{title}</h1>
           <p className="mb-4 whitespace-pre-wrap">{description}</p>
         </div>
       </section>
