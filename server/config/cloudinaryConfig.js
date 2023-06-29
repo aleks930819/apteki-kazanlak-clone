e63f01ba98ae90cac8c317778636c289107f0729
Aleksandar Atanasov <aleks930819@gmail.com>
2023-06-29 23:10:58 +0300

    change

--- server/config/cloudinaryConfig.js
+++ server/config/cloudinaryConfig.js
@@ -1,14 +1,10 @@
 import cloudinary from 'cloudinary';
 
-export const CLOUDINARY_CLOUD_NAME = 'dbrewse3d';
-export const CLOUDINARY_API_KEY = '397664564716853';
-export const CLOUDINARY_API_SECRET = 'NN7hwb0FzycQKKkPkhPfPoZCL9A';
-
 cloudinary.v2.config({
   secure: true,
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
